f9da979a8fde94469f05f50002e232e8a9a9d101
MichaelZalogin <zaloginmiha@gmail.com>
2022-06-14 20:30:36 +0300

    Lesson 2. Variables

---
+++ src/main/java/L2/Variables.java
@@ -0,0 +1,14 @@
+package L2;
+
+public class Variables {
+    public static void main(String[] args) {
+        int myInt = 434;
+        short myShort = 3266;
+        long myLong = 323242323L;
+        double myDouble = 434.32;
+        float myFloat = 32.54f;
+        char myChar = 'a';
+        boolean myBoolean = true;
+        byte myByte = 3; //-128 - +127;
+    }
+}
