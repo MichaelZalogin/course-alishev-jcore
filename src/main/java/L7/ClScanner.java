3c96069ac8e44ee83ba7a020a918268a5b9bddd8
MichaelZalogin <zaloginmiha@gmail.com>
2022-06-24 01:17:02 +0300

    Lesson 7. Class Scanner

---
+++ src/main/java/L7/ClScanner.java
@@ -0,0 +1,13 @@
+package L7;
+
+import java.util.*;
+
+public class ClScanner {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите целое число");
+        int x = s.nextInt();
+//        String string =  s.nextLine();
+        System.out.println("Вы ввели " + x);
+    }
+}
