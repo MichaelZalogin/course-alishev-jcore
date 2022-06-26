98184c372b430ee60f3f73641dc57f69ef035c8e
MichaelZalogin <zaloginmiha@gmail.com>
2022-06-26 23:43:12 +0300

    Lesson 8. While and do-while

--- src/main/java/L8/While.java
+++ src/main/java/L8/While.java
@@ -14,3 +14,15 @@ public class While {
         System.out.println("Вы ввели 5");
     }
 }
+
+class DoWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int value;
+        do {
+            System.out.println("Введите 5");
+            value = scanner.nextInt();
+        } while (value != 5);
+        System.out.println("Вы ввели 5");
+    }
+}
