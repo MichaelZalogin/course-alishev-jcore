731404abbb4f3af1e61967f580a4a44c4ddfc3a5
Michael Zalogin <zaloginmiha@gmail.com>
2022-07-03 16:11:35 +0300

    Lesson 13. Classes and objects

---
+++ src/main/java/L14/ClassesAndObjects.java
@@ -0,0 +1,27 @@
+package L14;
+
+public class ClassesAndObjects {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.name = "Роман";
+        person1.age = 50;
+
+        Person person2 = new Person();
+        person2.name = "Владимир";
+        person2.age = 34;
+        System.out.println();
+
+        System.out.println("Человека зовут " + person1.name);
+        System.out.println("Его возраст " + person1.age);
+    }
+}
+
+class Person {
+    /*
+    У класса могут быть:
+    1. Данные (поля);
+    2. Действия (методы);
+    */
+    String name;
+    int age;
+}
