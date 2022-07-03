1e5bf1bf4a9f613262ae44c3ee3a7c113dbcc569
Michael Zalogin <zaloginmiha@gmail.com>
2022-07-03 17:02:59 +0300

    Lesson 14. Methods

---
+++ src/main/java/L15/Methods.java
@@ -0,0 +1,19 @@
+package L15;
+
+public class Methods {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.name = "Иван";
+        person1.age = 34;
+        person1.speak();
+    }
+}
+
+class Person {
+    String name;
+    int age;
+
+    void speak() {
+        System.out.println("Меня зовут " + name + "мне " + age + " лет");
+    }
+}
