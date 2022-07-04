56542a739921ff3b1a20936de6bf3ce0d0a57d66
MichaelZalogin <zaloginmiha@gmail.com>
2022-07-04 23:49:26 +0300

    Lesson 19. This

---
+++ src/main/java/L19/Human.java
@@ -0,0 +1,35 @@
+package L19;
+
+public class Human {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void getInfo() {
+        System.out.println(this.name + " " + this.age);
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Human human1 = new Human();
+        human1.setName("Иван");
+        human1.setAge(24);
+        human1.getInfo();
+    }
+}
