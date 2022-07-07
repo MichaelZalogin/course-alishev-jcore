fa71fa3988bfe7ebde2e5a5949def148f115c595
MichaelZalogin <zaloginmiha@gmail.com>
2022-07-07 23:12:54 +0300

    Lesson 21. Static

---
+++ src/main/java/L21/Static.java
@@ -0,0 +1,36 @@
+package L21;
+
+public class Static {
+}
+
+class Human {
+    private int age;
+    private String name;
+
+    public Human(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Human human1 = new Human(24, "Bob");
+    }
+}
