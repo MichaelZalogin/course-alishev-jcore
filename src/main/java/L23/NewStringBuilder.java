7e1d9fa222e470278cb83e6ded172a7ce1536216
MichaelZalogin <zaloginmiha@gmail.com>
2022-07-08 01:49:09 +0300

    Lesson 23. StringBuilder

---
+++ src/main/java/L23/NewStringBuilder.java
@@ -0,0 +1,16 @@
+package L23;
+
+public class NewStringBuilder {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = " my";
+        String s3 = " friend";
+        String stringAll = s1 + s2 + s3;
+        System.out.println(stringAll);
+        StringBuilder sb1 = new StringBuilder("");
+        StringBuilder sb2 = new StringBuilder("");
+        StringBuilder sb3 = new StringBuilder("");
+        StringBuilder sb4 = new StringBuilder(s1 + s2 + s3);
+        System.out.println(sb4);
+    }
+}
