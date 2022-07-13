b7220dc958e45d97ac94d588a41770dffa07ef77
MichaelZalogin <zaloginmiha@gmail.com>
2022-07-14 01:23:01 +0300

    Lesson 28. AccessModifiers

---
+++ src/main/java/L28/AccessModifiers.java
@@ -0,0 +1,18 @@
+package L28;
+
+public class AccessModifiers {
+    public int id;
+    protected int s3;
+    int s2;
+    private int s1;
+
+    private void technicalMethod() {
+    }
+
+    public static void main(String[] args) {
+        //public (доступно в пределах проекта),
+        //private (доступно только в теле класса),
+        //default (доступно в пределах пакета),
+        //protected (доступно в пределах пакета и у сабклассов)
+    }
+}
